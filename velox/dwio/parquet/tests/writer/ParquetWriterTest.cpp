5dbc3749fd57ae572927da552fee592c4201549b
Rui Mo <rui@apache.org>
2025-07-30 16:40:54 +0100

    Add metric

--- velox/dwio/parquet/tests/writer/ParquetWriterTest.cpp
+++ velox/dwio/parquet/tests/writer/ParquetWriterTest.cpp
@@ -81,6 +81,7 @@ class ParquetWriterTest : public ParquetTestBase {
   };
 
   inline static const std::string kHiveConnectorId = "test-hive";
+  dwio::common::ColumnReaderStatistics stats;
 };
 
 std::vector<CompressionKind> params = {
@@ -144,7 +145,8 @@ TEST_F(ParquetWriterTest, dictionaryEncodingWithDictionaryPageSize) {
               std::move(inputStream),
               *leafPool_,
               colChunkPtr.compression(),
-              colChunkPtr.totalCompressedSize());
+              colChunkPtr.totalCompressedSize(),
+              stats);
           return pageReader->readPageHeader();
         }
         constexpr int64_t kFirstDataPageCompressedSize = 1291;
@@ -160,7 +162,8 @@ TEST_F(ParquetWriterTest, dictionaryEncodingWithDictionaryPageSize) {
             std::move(inputStream),
             *leafPool_,
             colChunkPtr.compression(),
-            colChunkPtr.totalCompressedSize());
+            colChunkPtr.totalCompressedSize(),
+            stats);
         return pageReader->readPageHeader();
       };
 
@@ -312,7 +315,8 @@ TEST_F(ParquetWriterTest, dictionaryEncodingOff) {
             std::move(inputStream),
             *leafPool_,
             colChunkPtr.compression(),
-            colChunkPtr.totalCompressedSize());
+            colChunkPtr.totalCompressedSize(),
+            stats);
         return pageReader->readPageHeader();
       };
 
@@ -479,7 +483,8 @@ TEST_F(ParquetWriterTest, testPageSizeAndBatchSizeConfiguration) {
             std::move(inputStream),
             *leafPool_,
             colChunkPtr.compression(),
-            colChunkPtr.totalCompressedSize());
+            colChunkPtr.totalCompressedSize(),
+            stats);
         return pageReader->readPageHeader();
       };
 
@@ -626,7 +631,8 @@ TEST_F(ParquetWriterTest, toggleDataPageVersion) {
             std::move(inputStream),
             *leafPool_,
             colChunkPtr.compression(),
-            colChunkPtr.totalCompressedSize());
+            colChunkPtr.totalCompressedSize(),
+            stats);
 
         return pageReader->readPageHeader().type;
       };
